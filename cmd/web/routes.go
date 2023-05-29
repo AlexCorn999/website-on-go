432b09dd378be3d01f1cc349cca58863e0b58dd6
AlexCorn999 <alexcorn999@list.ru>
2023-05-29 10:46:24 +0300

    added middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,13 +5,17 @@ import (
 
 	"github.com/AlexCorn999/website-on-go/pkg/config"
 	"github.com/AlexCorn999/website-on-go/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }

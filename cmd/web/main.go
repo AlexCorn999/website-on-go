44aa0fb42a60e6e152e48c9ea514f362a37c5426
AlexCorn999 <alexcorn999@list.ru>
2023-05-29 11:53:43 +0300

    added session cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/AlexCorn999/website-on-go/pkg/config"
 	"github.com/AlexCorn999/website-on-go/pkg/handlers"
 	"github.com/AlexCorn999/website-on-go/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
+var app config.AppConfig
 var portNumber = ":8080"
+var session *scs.SessionManager
 
 func main() {
+	// change this to true when in production
+	app.InProduction = false
 
-	var app config.AppConfig
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
